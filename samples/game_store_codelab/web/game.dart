0d93b45b54237e440d36eb165fffbad833cebe3e
Drone Build <support@drone.io>
2014-08-11 21:15:43 +0000

    'Step user-story-1'

---
+++ samples/game_store_codelab/web/game.dart
@@ -0,0 +1,13 @@
+import 'package:polymer/polymer.dart';
+import 'models.dart';
+
+@CustomTag('x-game')
+class XGame extends PolymerElement {
+  XGame.created() : super.created();
+  
+  Game game = new Game.sample();
+  
+  String uppercase(String value) => value.toUpperCase();
+  String stars(int rating) => new List.generate(rating, (_) => "\u2605").join();
+}
+
