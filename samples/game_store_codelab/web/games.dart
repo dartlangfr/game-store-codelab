68594a088fa2c61a6ef3531fabb6c7036f7c87f4
Drone Build <support@drone.io>
2014-08-11 21:15:43 +0000

    'Step user-story-3'

--- samples/game_store_codelab/web/games.dart
+++ samples/game_store_codelab/web/games.dart
@@ -1,3 +1,4 @@
+import 'dart:html';
 import 'package:polymer/polymer.dart';
 import 'models.dart';
 import 'services.dart';
@@ -7,5 +8,25 @@ class XGames extends PolymerElement {
   XGames.created() : super.created();
   
   List<Game> games = gameStoreService.games;
+  
+  // FILTERING
+  @observable String search = "";
+  
+  filterSearch(String search) => (Iterable games) => games.where((Game e) => e.contains(search));
+
+  // SORTING
+  @observable String sortField = "";
+  @observable bool sortAscending = true;
+  
+  sort(Event e, var detail, Element target) {
+    var field = target.dataset['field'];
+    sortAscending = field == sortField ? !sortAscending : true;
+    sortField = field;
+  }
+
+  sortBy(String field, bool ascending) => (Iterable games) {
+    var list = games.toList()..sort(Game.getComparator(field));
+    return ascending ? list : list.reversed;
+  };
 }
 
--- samples/game_store_codelab/web/models.dart
+++ samples/game_store_codelab/web/models.dart
@@ -11,5 +11,18 @@ class Game {
   // CONSTRUCTORS
   Game(this.id, this.name, this.genre, this.description, this.image, this.rating);
   Game.sample() : this(null, "Game name", "Game genre", "Game description", "chess.jpg", 4);
+
+  // Used for FILTERING
+  bool contains(String search) {
+    var pattern = new RegExp(search, caseSensitive: false);
+    return name.contains(pattern) || genre.contains(pattern) || description.contains(pattern);
+  }
+  
+  // Used for SORTING
+  static getComparator(String field) => _comparators.containsKey(field) ? _comparators[field] : (a, b) => 0;
+  static final Map _comparators = {
+    "name": (Game a, Game b) => a.name.compareTo(b.name),
+    "rating": (Game a, Game b) => a.rating.compareTo(b.rating)
+  };
 }
 
